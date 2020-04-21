e0c001fb07be4c9b0f4637f7291517a99596dee8
Diego Moraes <diego@cia.sinosserra.com.br>
2020-04-21 15:59:56 -0300

    Teste

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Primeiro projeto de Diego Moraes. Não tem problema";
 	}
 
 }
